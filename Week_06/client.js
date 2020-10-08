333106d6b058eb97a76837485402428c4c58e176
geek_ice <492663634@qq.com>
2020-10-08 11:42:51 +0800

    完成send函数

---
+++ Week_06/client.js
@@ -0,0 +1,16 @@
+class ResponseParser {
+    constructor() {}
+    receive(string) {
+        for (let i = 0; i < string.length; i++) {
+            this.receiveChar(string.charAt(i));
+        }
+    }
+    receiveChar(char) {}
+
+    send() {
+        return new Promise((resolve, reject) => {
+            const parser = new ResponseParser;
+            resolve("");
+        });
+    }
+}
